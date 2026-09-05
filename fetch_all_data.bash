72e38a0697b3b5d8d5dc311ec9e299b4e7171847
Leon M4 <leonm4@MacBook-Air-Leon.local>
2026-09-05 07:14:10 +0300

    Add las data

--- fetch_all_data.bash
+++ fetch_all_data.bash
@@ -16,7 +16,9 @@ xlsx="$DIR/XLSX"
 cd ..
 
 find . -path "./$DIR_NAME" -prune -o -type f -name "*.las" -exec cp {} "$las" \;
+find . -path "./$DIR_NAME" -prune -o -type f -name "*.LAS" -exec cp {} "$las" \;
 find . -path "./$DIR_NAME" -prune -o -type f -name "*.dlis" -exec cp {} "$dlis" \;
+find . -path "./$DIR_NAME" -prune -o -type f -name "*.DLIS" -exec cp {} "$dlis" \;
 find . -path "./$DIR_NAME" -prune -o -type f -name "*.csv" -exec cp {} "$csv" \;
 find . -path "./$DIR_NAME" -prune -o -type f -name "*.xlsx" -exec cp {} "$xlsx" \;
 
