0296b14a567087ad125d7025a786019d39bcb410
Leon M4 <leonm4@MacBook-Air-Leon.local>
2026-09-05 06:30:38 +0300

    Add Data to ML

---
+++ fetch_all_data.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Set way to dir
+DIR=$(pwd)
+DIR_NAME=$(basename "$DIR")
+
+#Check and create dir
+mkdir -p LAS DLIS CSV XLSX
+
+#Set ways to data dirs
+las="$DIR/LAS"
+dlis="$DIR/DLIS"
+csv="$DIR/CSV"
+xlsx="$DIR/XLSX"
+
+cd ..
+
+find . -path "./$DIR_NAME" -prune -o -type f -name "*.las" -exec cp {} "$las" \;
+find . -path "./$DIR_NAME" -prune -o -type f -name "*.dlis" -exec cp {} "$dlis" \;
+find . -path "./$DIR_NAME" -prune -o -type f -name "*.csv" -exec cp {} "$csv" \;
+find . -path "./$DIR_NAME" -prune -o -type f -name "*.xlsx" -exec cp {} "$xlsx" \;
+
+cd "$DIR"
